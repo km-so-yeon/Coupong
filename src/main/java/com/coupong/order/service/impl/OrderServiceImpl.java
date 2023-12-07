cd8af5a04a293052bcc6a5b5a6ba86438b165ff0
km-so-yeon <yeonsokm@gmail.com>
2023-12-07 11:05:07 +0900

    [update] dto 수정, 주석 삭제

--- src/main/java/com/coupong/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/coupong/order/service/impl/OrderServiceImpl.java
@@ -94,16 +94,14 @@ public class OrderServiceImpl implements OrderService {
             throw new BadRequestException(new BaseResponse(NO_ITEM_TO_ORDER));
         }
         for(OrderItemDto orderItemDto : orderDto.getOrderItems()) {
-            // TODO : 상품 옵션 추가로 확인해야 함
+            // TODO : 상품 옵션 추가로 확인
             Item item = itemRepository.findByRid(orderItemDto.getItemRid());
             // 상품을 주문할 수 있는지 확인
             canOrder(item, orderItemDto.getQuantity());
             item.order(orderItemDto.getQuantity());
 
             // 상품 쿠폰 사용
-            // TODO : 쿠폰 사용 시 쿠폰 종류 넘기기(쿠폰서비스 수정 필요)
             IssuedCoupon itemCoupon = couponService.useCoupon(orderItemDto.getIssuedCouponId(), CouponKind.ITEM);
-            //IssuedCoupon itemCoupon = couponService.useCoupon(orderItemDto.getIssuedCouponId(), CouponType.ITEM);
 
             int couponAppAmt = getCouponAppAmt(item, itemCoupon.getCoupon());
             OrderItem orderItem = OrderItem.createOrderItem(item, orderItemDto.getQuantity(), itemCoupon, couponAppAmt);
@@ -112,7 +110,6 @@ public class OrderServiceImpl implements OrderService {
 
         // 장바구니 쿠폰 사용
         IssuedCoupon basketCoupon = couponService.useCoupon(orderDto.getIssuedCouponId(), CouponKind.BASKET);
-        //IssuedCoupon basketCoupon = couponService.useCoupon(orderDto.getIssuedCouponId(), CouponType.BASKET);
 
         // 주문 생성
         // TODO 배송비 어떻게 할지 정해야 함
@@ -157,7 +154,7 @@ public class OrderServiceImpl implements OrderService {
             // 주문하려는 수량보다 적은지 확인
             throw new BadRequestException(new BaseResponse(OUT_OF_STOCK));
         }
-        // 상품 옵션 확인 필요
+        // TODO 상품 옵션 확인 필요
 
     }
 
